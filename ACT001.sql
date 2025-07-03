f94a0d6a5f59acf55b8db8c207aa1cdbac63eb0d
Ryan Espinosa <159238088+Ryanezpi@users.noreply.github.com>
2025-07-03 11:32:14 +0800

    Create ACT001.sql

---
+++ ACT001.sql
@@ -0,0 +1,34 @@
+--problem 1
+
+CREATE TABLE author (
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    id_number int(10),
+    dob date
+    );
+
+CREATE TABLE publishers (
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    address VARCHAR(100)
+    );
+
+CREATE TABLE books (
+    title_name VARCHAR(100),
+    ISBN_number int(10),
+    publication date,
+    price decimal(10,2)
+    );
+
+--problem 2
+
+
+CREATE TABLE patient (
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    id_number int(10),
+    phone_number int(10),
+    dob date
+    );
+
+
